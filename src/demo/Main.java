63861510a8e73d32dfacc7336db2a9e1c3a47205
haotran7902 <130350601+haotran7902@users.noreply.github.com>
2025-01-14 23:03:20 +0700

    abc

--- src/demo/Main.java
+++ src/demo/Main.java
@@ -6,6 +6,10 @@ public class Main {
 	public static int tinhTong(int a,int b){
 		return a+b;
 	}
+	
+	public static int hieu(int a,int b){
+		return a-b;
+	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
